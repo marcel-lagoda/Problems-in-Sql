1bfbe3add9e7264d784b4e39c6c5d6b722971b6a
Marcel <marceli.lagoda@gmail.com>
2020-12-17 02:30:43 +0100

    Add other-problems northwind db

---
+++ Northwind_dbo/northwind-other-problems.sql
@@ -0,0 +1,163 @@
+-- 1 Categorize customers into groups, based on the number of their orders.
+-- 1.1
+select distinct Customers.CustomerID
+              , CompanyName
+              , NumberOfOrders   = count(OrderID) over (partition by O.CustomerID)
+              , OrdersTreshholds = (
+    case
+        when count(OrderID) over (partition by O.CustomerID) >= 15 then 'High'
+        when count(OrderID) over (partition by O.CustomerID) < 15
+            and count(OrderID) over (partition by O.CustomerID) >= 10 then 'Medium'
+        when count(OrderID) over (partition by O.CustomerID) < 10 then 'low'
+        end
+    )
+from Customers
+         join Orders O on Customers.CustomerID = O.CustomerID
+order by NumberOfOrders desc;
+
+select distinct Customers.CustomerID
+              , CompanyName
+              , NumberOfOrders =
+    (case
+         when count(OrderID) >= 15 then 'High'
+         when count(OrderID) < 15 then 'Low'
+        end)
+from Customers
+         join Orders on Customers.CustomerID = Orders.CustomerID
+group by Customers.CustomerID, CompanyName
+order by NumberOfOrders;
+
+
+with CTE as (
+    select Customers.CustomerID
+         , CompanyName
+         , NumberOfOrders = count(OrderID)
+    from Customers
+             join Orders O on Customers.CustomerID = O.CustomerID
+)
+select CTE.CompanyName
+     , CTE.CompanyName
+     , CTE.NumberOfOrders =
+       (case end)
+
+
+------------------------------------------
+-- 2. Assign a sequential number to each row.
+-- 2.1. Very high overhead (using subquery).
+select o1.CustomerID
+     , o1.OrderID
+     , (
+    select count(*)
+    from Orders o2
+    where o2.OrderID <= o1.OrderID
+      and o2.CustomerID = o1.CustomerID
+) as RowNum
+from Orders o1
+order by CustomerID, RowNum;
+
+-- 2.2. using row_number() function
+select CustomerID
+     , OrderID
+     , row_number() over (partition by CustomerID order by OrderID) as RowNum
+from Orders;
+
+
+-----------------------------------------------------------------------------
+-- 3. Create collections
+select C1.Country
+     , stuff((
+                 select distinct ',' + C2.City
+                 from dbo.Customers C2
+                 where C2.Country = C1.Country
+                 for xml path('')
+             ), 1, 1, '') as Cities
+from dbo.Customers C1
+group by C1.Country;
+
+
+----------------------------------------------
+-- 4. Select the very last order for each customer.
+-- 4.1. Correlated where sub-query.
+select *
+--        [CustomerID]
+--      , [OrderID]
+from Orders o1
+where [OrderID] = (
+    select max([OrderID])
+    from Orders o2
+    where o1.[CustomerID] = o2.[CustomerID]
+)
+order by [CustomerID];
+
+-- 4.2. max() + group by (limited data)
+select CustomerID
+     , max(OrderID)
+from Orders
+group by CustomerID;
+
+-- 4.3. Correlated select sub-query.
+select *
+--        CustomerID
+--      , OrderID
+     , (
+    select distinct max(OrderID)
+    from Orders o2
+    where o2.CustomerID = o1.CustomerID
+) as LastOrderID
+from Orders o1
+
+-- 4.4. Independent joined sub-query.
+select
+--        *
+    Orders.CustomerID
+     , OrderID
+from Orders
+         join
+     (
+         select CustomerID
+              , max(OrderID) as LastOrderID
+         from Orders
+         group by CustomerID
+     ) as LastOrders
+     on OrderID = LastOrders.LastOrderID;
+
+-- 4.5. Window function (high overhead).
+select distinct CustomerID
+              , max(OrderID) over (partition by CustomerID order by OrderID desc) as LastOrderID
+from Orders;
+------------------------------------------------------------------------------------------------
+
+select OrderID,
+       CustomerID,
+       EmployeeID,
+       OrderDate,
+       RequiredDate,
+       ShippedDate,
+       ShipVia,
+       Freight,
+       ShipName,
+       ShipAddress,
+       ShipCity,
+       ShipRegion,
+       ShipPostalCode,
+       ShipCountry,
+       (
+           select count(*)
+           from Orders o2
+           where o2.CustomerID = o1.CustomerID
+       ) as NumberOfOrders
+from Orders o1
+
+
+select stuff(FirstName + N' ' + LastName, 1, charindex(N' ', FirstName + N' ' + LastName) - 1,
+             replicate(N'X', len(FirstName))) as HiddenName
+from dbo.Employees
+
+USE Northwind
+GO
+
+SELECT 'Hired : ' +  CAST (HireDate as varchar(10)) as konwersjaCast,
+        CONVERT(varchar(10),HireDate, 113) as KonwersjaConvertStyles,
+        CAST(CAST (HireDate as date) as varchar(10)) as DblCast,
+        HireDate
+FROM dbo.Employees
